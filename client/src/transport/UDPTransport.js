2042f62c9a49aed289da2aea869f740051717e18
wangchuan <wangchuan3533@gmail.com>
2016-10-14 22:28:34 +0800

    update

--- client/src/transport/UDPTransport.js
+++ client/src/transport/UDPTransport.js
@@ -1,16 +1,21 @@
 import dgram from 'dgram'
 const sock = dgram.createSocket('udp4')
 const message = Buffer.from('Some bytes')
+const HOST = 'ec2-54-175-246-94.compute-1.amazonaws.com'
+//const HOST = '10.0.4.113'
+const PORT = 11111
+//const HOST = 'localhost'
 
 export default {
   connect: function() {
-    sock.send('hello', 11111, 'localhost', (err) => {
+    //sock.bind(22222)
+    sock.send('hello', PORT, HOST, (err) => {
       if (err) console.log(err)
     })
   },
   
   send: function(msg) {
-    sock.send(msg, 11111, 'localhost', (err) => {
+    sock.send(msg, PORT, HOST, (err) => {
       if (err) console.log(err)
     })
   },
